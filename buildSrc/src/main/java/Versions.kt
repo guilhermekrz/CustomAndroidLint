c5e993c4de467f14d6f10604f6650e33db6342cb
Guilherme Krzisch <gkrzisch@schoology.com>
2020-05-29 18:43:08 -0300

    First commit from https://github.com/fabiocarballo/lint-sample

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,11 @@
+object Versions {
+
+    const val androidAppCompat = "1.0.2"
+    const val androidGradlePlugin = "3.5.3"
+    const val androidKtxCore = "1.0.2"
+    const val kotlin = "1.3.61"
+    const val lint = "26.5.3"
+    const val junitJupiter = "5.5.0"
+    const val junitVintage = "5.5.0"
+    const val jUnit = "4.12"
+}
